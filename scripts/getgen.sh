fd959d074f7a2bc38558f9954b713820b951297f
Mikhail Kuznetsov <mikhail.kuznetsov@gotbit.io>
2024-04-09 19:05:20 +0700

    ref: update ABI

---
+++ scripts/getgen.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./opera --datadir ./datadir/datadir_opera1 export genesis genesis.g
--- scripts/new_network.sh
+++ scripts/new_network.sh
@@ -2,11 +2,9 @@
 
 validutorsNum=5
 
-#../build/opera \
-#network new $validutorsNum \
-#--datadir ./datadir/datadir_opera \
-#--network.type=devnet \
-#--password ./pass.txt \
-#--validatorsfile ./validators.txt
-
-../build/opera --datadir ./datadir/datadir_opera1 export genesis genesis.g
+../build/opera \
+network new $validutorsNum \
+--datadir ./datadir/datadir_opera \
+--password ./pass.txt \
+--validatorsfile ./validators.txt \
+--network.type devnet
